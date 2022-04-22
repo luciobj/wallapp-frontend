826e3fac510785b4d7cbed0d0e710ab4d936bfc8
Lúcio BJ <lbjunq@hotmail.com>
2022-04-22 19:35:55 -0300

    feat: added router for each page

---
+++ src/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+      <Route element={<NotFound />} />
+    </Routes>
+  );
+}
