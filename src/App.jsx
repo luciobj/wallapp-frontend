e40e877a9c87e472017231d837029777f0961e72
Lúcio BJ <lbjunq@hotmail.com>
2022-04-22 20:27:09 -0300

    chore: added providers in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import PostItProvider from './context/PostItProvider';
+import UserProvider from './context/UserProvider';
 import Router from './Router';
 
 function App() {
   return (
-    <Router />
+    <BrowserRouter>
+      <UserProvider>
+        <PostItProvider>
+          <Router />
+        </PostItProvider>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
